239e6f4018b78897fc731b337a3e667676616888
Everton Vieira <evertonvieira1994@gmail.com>
2020-12-15 21:49:20 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,69 +1,48 @@
-#Author: Suzana Viana Mota
-#Este algoritmo classifica gatos e cachorros utilizando
-#Tecnicas de Machine Learning Naive Bayes
+from sklearn.datasets import load_breast_cancer
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import accuracy_score
+import seaborn as sns
+import pandas as pd
+import numpy as np
 
+# Carregar o dataset
+data = load_breast_cancer()
 
+# Organizar nossos dados
+label_names = data['target_names']
+labels = data['target']
+feature_names = data['feature_names']
+features = data['data']
 
-######################## 1 MONTANDO DATASET   ################################################  
-#Caracteristicas
-#e fofinho?
-#Tem uma orelhinha pequena?
-#Faz miau?
+frame = pd.DataFrame(np.c_[data['data'], data['target']],
+                  columns= np.append(data['feature_names'], ['target']))
 
-#Se SIM = 1 / Se NAO = 0
+df = frame.melt(['target'], var_name='cols',  value_name='vals')
+g = sns.FacetGrid(df, col='cols', hue="target", palette="Set1", col_wrap=4)
+g = (g.map(sns.distplot, "vals", hist=True, ))
 
-bichinho1 = [1, 1, 1]
-bichinho2 = [1, 0, 1]
-bichinho3 = [0, 1, 1]
-bichinho4 = [1, 1, 0]
-bichinho5 = [0, 1, 0]
-bichinho6 = [0, 1, 0]
+# Olhando para os nossos dados
+print(label_names)
+print('Class label = ', labels[0])
+print(feature_names)
+print(features[0])
 
-dados = [bichinho1, bichinho2, bichinho3, bichinho4, bichinho5, bichinho6]
+# Dividir nossos dados
+train, test, train_labels, test_labels = train_test_split(features,
+                                                          labels,
+                                                          test_size=0.33,
+                                                          random_state=42)
 
-#Se GATO = 1 / Se CACHORRO = -1
-marcacoes = [1, 1, 1, -1, -1, -1]
+# Inicializar nosso classificador
+gnb = GaussianNB()
 
-######################## 2 CRIANDO MODELO   ################################################  
-from sklearn.naive_bayes import MultinomialNB
+# Treinar nosso classificador
+model = gnb.fit(train, train_labels)
 
-modelo = MultinomialNB()
-modelo.fit(dados, marcacoes)
+# Fazer previsões
+preds = gnb.predict(test)
+print(preds)
 
-######################## 3 FAZENDO PREDICOES   ################################################ 
-bicho_misterioso1 = [1, 1, 1]
-bicho_misterioso2 = [1, 0, 0]
-bicho_misterioso3 = [0, 0, 1]
-
-teste = [bicho_misterioso1, bicho_misterioso2, bicho_misterioso3]
-
-marcacoes_teste = [1, -1, 1]
-
-######################## 4 OBSERVANDO RESULTADOS   ################################################ 
-resultado = modelo.predict(teste)
-
-diferencas = resultado - marcacoes_teste
-
-acertos = [d for d in diferencas if d == 0]
-
-total_de_acertos = len(acertos)
-
-
-total_de_elementos = len(teste)
-
-taxa_de_acerto = 100.0 * total_de_acertos / total_de_elementos
-
-print("1: Gato e -1: Cachorro")
-
-print("Resultado: ")
-print(resultado)
-
-print ("Marcacoes: ")
-print(marcacoes_teste)
-
-print("Diferencas: ")
-print(diferencas)
-
-
-print("Taxa de acerto do Algoritmo: ")
-print(taxa_de_acerto)
+# Avaliar a precisão
+print(accuracy_score(test_labels, preds))
